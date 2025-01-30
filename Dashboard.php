9e98175b88a7c82e2000a9a9039e23bb2d5d2ca3
odesagovori <og69937@ubt-uni.net>
2025-01-30 15:09:14 +0100

    Ndërrimi i sessions

--- Dashboard.php
+++ Dashboard.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-if (!isset($_SESSION['email'])) {
+if (!isset($_SESSION['username'])) {
     header("Location: Login.php");
 } else {
     if ($_SESSION['role'] == "user") {
