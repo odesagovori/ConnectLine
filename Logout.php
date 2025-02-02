0e054819263c6e8b819a669ef55063803d94ca47
erjonazejnullahu <ez70511@ubt-uni.net>
2025-02-02 19:55:56 +0100

    Rregullimi i Logout

--- Logout.php
+++ Logout.php
@@ -1,26 +1,9 @@
 <?php
-// Start the session if it's not already started
 session_start();
-
-// Clear the session and cookie data
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-}
-
-// Destroy the session
+if(isset($_SESSION['email'])){
 session_destroy();
-
-// Prevent the page from being cached by the browser
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: 0");
-
-// Redirect the user to the login page
-header("Location: Login.php");
-exit();
+header('Location:Login.php');
+}else{
+header('Location:Login.php');
+}
 ?>
-
-<script>
-window.history.replaceState({}, document.title, window.location.pathname);
-</script>
